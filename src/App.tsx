61e06def409277d498ca6d10aefa12fadc165b77
Gkikas <ngkdev93@gmail.com>
2024-10-25 10:38:05 +0300

    added test route

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageHome from "./components/PageHome.tsx";
 import Header from "./components/Header.tsx";
 import { Box, Container } from "@mui/material";
 
 function App() {
-
   return (
-      <BrowserRouter>
-        <Container sx={{display:'flex',alignItems:'center',width:'1400px',justifyContent:'space-between',height:'700px'}} maxWidth="xl">
-          <Header/>
-          <Box sx={{background:'#fff',width:'80%',height:'100%',display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <Routes>
-                <Route path={'/'} element={<PageHome/>}/>
-                <Route path={'/about'} element={<h1>about</h1>}/>
-                <Route path={'/services'} element={<h1>services</h1>}/>
-                <Route path={'/Portfolio'} element={<h1>portfolio</h1>}/>
-                <Route path={'/contact'} element={<h1>contact</h1>}/>
-            </Routes>
-          </Box>
-        </Container>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          width: "1400px",
+          justifyContent: "space-between",
+          height: "700px",
+        }}
+        maxWidth="xl"
+      >
+        <Header />
+        <Box
+          sx={{
+            background: "#fff",
+            width: "80%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Routes>
+            <Route path={"/"} element={<PageHome />} />
+            <Route path={"/about"} element={<h1>about</h1>} />
+            <Route path={"/services"} element={<h1>services</h1>} />
+            <Route path={"/Portfolio"} element={<h1>portfolio</h1>} />
+            <Route path={"/contact"} element={<h1>contact</h1>} />
+            <Route path={"/test"} element={<h1>contact</h1>} />
+          </Routes>
+        </Box>
+      </Container>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
